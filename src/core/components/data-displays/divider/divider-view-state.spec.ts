14027ab1402a448adf645f35fc38a7590641a609
Gabriel Medeiros Souza <gabriel@Nabucodonosor>
2023-01-13 18:28:15 -0300

    test: implement component divider-view-state tests

--- src/core/components/data-displays/divider/divider-view-state.spec.ts
+++ src/core/components/data-displays/divider/divider-view-state.spec.ts
@@ -7,4 +7,25 @@ describe('Component: Divider', () => {
     expect(sut.orientation).toBe('horizontal');
     expect(sut.size).toBe(100);
   });
+
+  it.each([
+    { size: -1, expected: 0 },
+    { size: 0, expected: 0 },
+    { size: 50, expected: 50 },
+    { size: 100, expected: 100 },
+    { size: 101, expected: 100 }
+  ])("should set size to a value between 0 and 100: (size: $size) => expected: $expected", ({ size, expected }) => {
+    const sut = new DividerViewState({ size });
+
+    expect(sut.size).toBe(expected);
+  });
+
+  it.each([
+    "horizontal",
+    "vertical"
+  ] as const)(`should set orientation to %s`, (orientation) => {
+    const sut = new DividerViewState({ orientation });
+
+    expect(sut.orientation).toBe(orientation);
+  });
 });
