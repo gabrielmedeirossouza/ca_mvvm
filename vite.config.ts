fcf560cba06cecd3e0aa46b1d5f397ca79fad976
Gabriel Medeiros Souza <gabriel@Nabucodonosor>
2023-01-14 10:14:36 -0300

    chore: add jsdom and integrate it with vitest to test react components

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import alias from "@rollup/plugin-alias";
 import path from "node:path";
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-  },
   plugins: [alias(), react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }]
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'tests/setup.ts',
   },
 });
